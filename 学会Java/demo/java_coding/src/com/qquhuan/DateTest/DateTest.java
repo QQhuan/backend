4757c89d2d2db8ffc98b51ba3284d99af3122e45
QQhuan <469558400@qq.com>
2024-04-07 21:34:36 +0800

    :memo:code:(java基础)SimpleDateFormat类

--- 学会Java/demo/java_coding/src/com/qquhuan/DateTest/DateTest.java
+++ 学会Java/demo/java_coding/src/com/qquhuan/DateTest/DateTest.java
@@ -1,5 +1,6 @@
 package com.qquhuan.DateTest;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTest {
@@ -21,5 +22,12 @@ public class DateTest {
 
         long t = now.getTime();
         System.out.println(t);
+
+        // SimpleDateFormat类 用于日期格式化
+        SimpleDateFormat format = new SimpleDateFormat();
+
+        SimpleDateFormat format2 = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        Date d3 = new Date();
+        System.out.println(format.format(d3));
     }
 }
